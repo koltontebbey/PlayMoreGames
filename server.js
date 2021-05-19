6ff17720ae68a622f165bda54a6c72ac207698d9
Kolton <koltontebbey@gmail.com>
2021-05-19 19:30:39 -0400

    Implemented basic "Add User" functionality

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ app.get('/listUsers', function (req, res) {
    });
 })
 
+app.get('/example', function (req, res) {
+    res.send('Hello from A!')
+    
+  })
+
 app.post('/addUser', function (req, res) {
     // First read existing users.
     fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
@@ -28,6 +33,35 @@ app.post('/addUser', function (req, res) {
     });
  })
  
+ app.get('/editUser', function (req, res) {
+     res.send('Edit  page \
+     <form action="addUser" method="post"> \
+  <label for="fname">First name:</label><br> \
+  <input type="text" id="fname" name="fname"><br> \
+  <label for="lname">Last name:</label><br> \
+  <input type="text" id="lname" name="lname"> \
+  <input type="submit" value="Submit"> \
+</form>\
+     <br /><br /> <br />  brokey wokey')
+    //
+    // First read existing users.
+    // fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+    //    data = JSON.parse( data );
+    //    data["user4"] = user["user4"];
+    //    console.log( data );
+    //    res.end( JSON.stringify(data));
+    // });
+ })
+
+ app.post('/deleteUser', function (req, res) {
+
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       data = JSON.parse( data );
+       data["user4"] = user["user4"];
+       console.log( data );
+       res.end( JSON.stringify(data));
+    });
+ })
 
 var server = app.listen(8081, function () {
    var host = server.address().address
