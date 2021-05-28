4e7e86bfc4c1dec902a1f0870fe8db9021dd2528
Kolton <koltontebbey@gmail.com>
2021-05-28 17:03:51 -0400

    Broke the server.js :)

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # PlayMoreGames
 Bad games, worse developers.
 GG
+
+
+# Want to run this code?
+
+Lol, we didn't add any dependencies yet so gl 
--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
+app.use(express.urlencoded({
+   extended: true
+ }))
 
+ //console.log("Starting");
+//Hardcoded
 var user = {
     "user4" : {
        "name" : "mohit",
@@ -18,28 +23,64 @@ app.get('/listUsers', function (req, res) {
    });
 })
 
-app.get('/example', function (req, res) {
+app.get('/', function (req, res) {
     res.send('Hello from A!')
     
-  })
+})
 
-app.post('/addUser', function (req, res) {
-    // First read existing users.
-    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       data = JSON.parse( data );
-       data["user4"] = user["user4"];
-       console.log( data );
-       res.end( JSON.stringify(data));
-    });
+//Original add user function, deprecating this but saving for visibility for now
+// app.post('/addUser', function (req, res) {
+// // First read existing users.
+// fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+//    data = JSON.parse( data );
+//    //console.log(res);
+//    console.log(req.body.fname);
+//    data["user4"] = req.body;
+//    console.log( data );
+//    res.end( JSON.stringify(data));
+//    });
+// })
+
+
+
+//Actual add user function
+app.post('/addUserHandler', function (req, res) {
+   try {
+     
+      // First read existing users.
+      fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+         let users = [];
+         users = JSON.parse(data);
+         
+         console.log(users);
+         input = JSON.stringify(req.body);
+         console.log(input);
+         users.push(input);
+         //console.log(users);
+         //console.log( data );
+         //Let's append this to the data
+         let newData = JSON.stringify(users);
+         fs.writeFile("./users.json", newData, function(err) { if (err) throw err; })
+         res.end(console.log(data));
+      });
+   } catch (e) { console.log(e);}
  })
  
- app.get('/editUser', function (req, res) {
+
+
+
+ //This currently functions as an "Add user" will compartmentalize functionality soon
+ app.get('/addUser', function (req, res) {
      res.send('Edit  page \
-     <form action="addUser" method="post"> \
+     <form action="addUserHandler" method="post"> \
   <label for="fname">First name:</label><br> \
   <input type="text" id="fname" name="fname"><br> \
   <label for="lname">Last name:</label><br> \
-  <input type="text" id="lname" name="lname"> \
+  <input type="text" id="lname" name="lname"><br> \
+  <label for="username">Username:</label><br> \
+  <input type="text" id="username" name="username"><br> \
+  <label for="password">Password:</label><br> \
+  <input type="password" id="password" name="password"><br> \
   <input type="submit" value="Submit"> \
 </form>\
      <br /><br /> <br />  brokey wokey')
